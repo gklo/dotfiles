87c679896a9351289af52e9a77b33049bef5fcf3
Glaser Lo <glaser.k.lo@gmail.com>
2022-09-03 21:15:28 +0800

    update nvim hop config

--- nvim/lua/init_mapping.lua
+++ nvim/lua/init_mapping.lua
@@ -71,15 +71,11 @@ map { "n", "<C-e>", ":NvimTreeFindFileToggle<CR>" }
 -- hop
 map { "n", "<leader>s", "<cmd>HopChar2<cr>" }
 -- place this in one of your configuration file(s)
-map { "n", "f",
+map { "", "f",
   "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>" }
-map { "n", "F",
+map { "", "F",
   "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>" }
-map { "o", "f",
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>" }
-map { "o", "F",
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>" }
 map { "", "t",
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>" }
+  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true,  hint_offset = -1 })<cr>" }
 map { "", "T",
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>" }
+  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>" }
