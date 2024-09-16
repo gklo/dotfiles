a9b1ce4d9f62fa27f98e39bcd2848a010b47c7d3
gklo <glaser.k.lo@gmail.com>
2024-09-16 16:16:30 +0800

    add lsp file operation to nvim

--- nvim2/init.lua
+++ nvim2/init.lua
@@ -770,7 +770,19 @@ require("lazy").setup({
 		opts = {
 			-- configurations go here
 		},
-	}
+	},
+
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+	
 })
 
 -- commands
